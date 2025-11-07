790a18cdde33ae1b056673060798408cc33e4cb2
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-11-07 08:33:26 -0300

    withStartBlock

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -6,9 +6,13 @@ import "sim-idx-generated/Generated.sol";
 import "./BlockListener.sol";
 
 contract Triggers is BaseTriggers {
+    uint256 public constant BASE_START_BLOCK = 22576928;
+    uint256 public constant ETHEREUM_START_BLOCK = 21574092;
+
+
     function triggers() external virtual override {
         BlockListener listener = new BlockListener();
-        addTrigger(chainGlobal(Chains.Base), listener.triggerOnBlock());
-        addTrigger(chainGlobal(Chains.Ethereum), listener.triggerOnBlock());
+        addTrigger(chainGlobal(Chains.Base.withStartBlock(BASE_START_BLOCK)), listener.triggerOnBlock());
+        addTrigger(chainGlobal(Chains.Ethereum.withStartBlock(ETHEREUM_START_BLOCK)), listener.triggerOnBlock());
     }
 }
